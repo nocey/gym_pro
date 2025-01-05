b6816eb31ba77b90ff87c6cde98a539a78acd254
Rynaud <ygzozduygun@gmail.com>
2025-01-05 16:22:51 +0300

    FEAT: Adds button to backout from RegisterActivity to MainActivtiy

--- app/src/main/java/com/example/gympro/RegisterActivity.java
+++ app/src/main/java/com/example/gympro/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.gympro;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -12,7 +13,7 @@ public class RegisterActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private EditText registerEmailEditText, registerPasswordEditText;
-    private Button registerUserButton;
+    private Button registerUserButton,loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +21,14 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         mAuth = FirebaseAuth.getInstance();
-
+        loginButton = findViewById(R.id.loginButton);
         registerEmailEditText = findViewById(R.id.registerEmailEditText);
         registerPasswordEditText = findViewById(R.id.registerPasswordEditText);
         registerUserButton = findViewById(R.id.registerUserButton);
 
+
         registerUserButton.setOnClickListener(v -> registerUser());
+        loginButton.setOnClickListener(v -> startActivity(new Intent(RegisterActivity.this, MainActivity.class)));
     }
 
     private void registerUser() {
